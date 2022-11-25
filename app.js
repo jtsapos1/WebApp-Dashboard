8400d13d07fa42e1ed72dbe0b1f429f1c6895b12
jtsapos <jtsapos@gmx.com>
2022-11-24 20:57:00 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ alertBanner.innerHTML = `
 <div class="alert-banner">
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
     to complete</p>
-<p class="alert-banner-close"></p>
+<p class="alert-banner-close">x</p>
 </div>
 `;
 
