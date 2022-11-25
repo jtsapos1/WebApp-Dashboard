fb22a897bf29fcfc9d6899dd3700af0d3b2a9481
jtsapos <jtsapos@gmx.com>
2022-11-25 17:59:42 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -139,11 +139,11 @@ send.addEventListener('click', () => {
 //ensure user and message fields are filled out
 if (user.value === "" && message.value === "") {
 	alert("Please fill out user and message fields before sending");
-} else if (user.value === "") {
+} else if (user.value === "" ) {
 	alert("Please fill out user field before sending");
-} else if (message.value === "") {
+} else if (message.value === "" ) {
 	alert("Please fill out message field before sending");
 } else {
-	alert('Message successfully sent to: ${user.value}');
+	alert(`Message successfully sent to: ${user.value}`);
 }
 });
